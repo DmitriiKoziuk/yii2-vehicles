1919c6c92515e20bf045562ac1c0545481568b0e
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-30 14:47:15 +0200

    Create migration for dk_vehicle_transmissions table

---
+++ src/migrations/m190330_123655_create_dk_vehicle_transmissions_table.php
@@ -0,0 +1,58 @@
+<?php
+namespace DmitriiKoziuk\yii2Vehicles\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%dk_vehicle_transmissions}}`.
+ */
+class m190330_123655_create_dk_vehicle_transmissions_table extends Migration
+{
+    private $vehicleTransmissionTable = '{{%dk_vehicle_transmissions}}';
+    private $vehicleTransmissionManufactureTable = '{{%dk_vehicle_transmission_manufactures}}';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable($this->vehicleTransmissionTable, [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(45)->notNull(),
+            'transmission_manufacture_id' => $this->integer()->notNull(),
+            'type' => "ENUM('manual', 'automatic', 'cvt') NULL DEFAULT NULL",
+        ]);
+        $this->createIndex(
+            'idx_dk_vehicle_transmissions_transmission_manufacture_id',
+            $this->vehicleTransmissionTable,
+            'transmission_manufacture_id'
+        );
+        $this->createIndex(
+            'uidx_dk_vehicle_transmissions_name',
+            $this->vehicleTransmissionTable,
+            'name',
+            true
+        );
+        $this->addForeignKey(
+            'fk_dk_vehicle_transmissions_transmission_manufacture_id',
+            $this->vehicleTransmissionTable,
+            'transmission_manufacture_id',
+            $this->vehicleTransmissionManufactureTable,
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk_dk_vehicle_transmissions_transmission_manufacture_id',
+            $this->vehicleTransmissionTable
+        );
+        $this->dropTable($this->vehicleTransmissionTable);
+    }
+}
