f6ac998d2e2c8b47c08da921dc3daebc9a7d1612
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-30 15:14:52 +0200

    Generate CRUD for Transmission entity

---
+++ src/views/backend/transmission/_search.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model DmitriiKoziuk\yii2Vehicles\entities\TransmissionSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="transmission-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'transmission_manufacture_id') ?>
+
+    <?= $form->field($model, 'type') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
