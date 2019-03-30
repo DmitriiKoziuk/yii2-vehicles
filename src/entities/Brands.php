1234eae9b7e9fcdacd206ec897d7fa35d47f14b1
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-30 13:04:03 +0200

    Create entity Brands for dk_vehicle_brands table

---
+++ src/entities/Brands.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace DmitriiKoziuk\yii2Vehicles\entities;
+
+use Yii;
+use \yii\db\ActiveRecord;
+use DmitriiKoziuk\yii2Base\BaseModule;
+use DmitriiKoziuk\yii2Vehicles\VehiclesModule;
+
+/**
+ * This is the model class for table "{{%dk_vehicle_brands}}".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Brands extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%dk_vehicle_brands}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 25],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t(BaseModule::ID, 'ID'),
+            'name' => Yii::t(VehiclesModule::ID, 'Name'),
+        ];
+    }
+}
