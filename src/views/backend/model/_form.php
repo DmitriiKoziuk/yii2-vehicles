26246c436b7711131a7108147425341941a7de38
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-30 14:05:45 +0200

    Generate CRUD for entity Model

---
+++ src/views/backend/model/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model DmitriiKoziuk\yii2Vehicles\entities\Model */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="model-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'brand_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ src/views/backend/model/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\web\View;
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+use DmitriiKoziuk\yii2Vehicles\entities\ModelSearch;
+use DmitriiKoziuk\yii2Vehicles\entities\Model;
+
+/**
+ * @var $this View
+ * @var $searchModel ModelSearch
+ * @var $dataProvider ActiveDataProvider
+ */
+
+$this->title = Yii::t('app', 'Models');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="model-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Model'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            [
+                'attribute' => 'brand_id',
+                'content' => function ($entity) {
+                    /** @var $entity Model */
+                    return $entity->brand->name;
+                }
+            ],
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
