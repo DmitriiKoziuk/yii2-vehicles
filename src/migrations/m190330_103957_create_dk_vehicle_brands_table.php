feae9b3deb3563cbfe0da4a51763140510550a23
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-30 13:00:47 +0200

    Create migration for dk_vehicle_brands table

---
+++ src/migrations/m190330_103957_create_dk_vehicle_brands_table.php
@@ -0,0 +1,37 @@
+<?php
+namespace DmitriiKoziuk\yii2Vehicles\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%dk_vehicle_brands}}`.
+ */
+class m190330_103957_create_dk_vehicle_brands_table extends Migration
+{
+    private $vehicleBrandsTable = '{{%dk_vehicle_brands}}';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable($this->vehicleBrandsTable, [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(25)->notNull(),
+        ]);
+        $this->createIndex(
+            'idx_dk_vehicle_brands_name_unique',
+            $this->vehicleBrandsTable,
+            'name',
+            true
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable($this->vehicleBrandsTable);
+    }
+}
