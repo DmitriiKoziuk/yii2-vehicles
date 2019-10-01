6642fd3a50e2f5a04001c210a99a34c3184fdac8
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-10-01 18:12:06 +0300

    ModuleInitService::registerModule renamed to ModuleRegistrationService::addModule Some code style fix

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,17 +1,26 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace DmitriiKoziuk\yii2Vehicles;
 
 use Yii;
+use yii\base\Application;
 use yii\base\BootstrapInterface;
-use DmitriiKoziuk\yii2ModuleManager\services\ModuleInitService;
+use yii\base\InvalidConfigException;
+use yii\di\NotInstantiableException;
+use DmitriiKoziuk\yii2ModuleManager\services\ModuleRegistrationService;
 use DmitriiKoziuk\yii2ConfigManager\ConfigManagerModule;
 use DmitriiKoziuk\yii2ConfigManager\services\ConfigService;
 
 final class Bootstrap implements BootstrapInterface
 {
+    /**
+     * @param Application $app
+     * @throws InvalidConfigException
+     * @throws NotInstantiableException
+     */
     public function bootstrap($app)
     {
-        ModuleInitService::registerModule(VehiclesModule::class, function () {
+        ModuleRegistrationService::addModule(VehiclesModule::class, function () {
             /** @var ConfigService $configService */
             $configService = Yii::$container->get(ConfigService::class);
             return [
